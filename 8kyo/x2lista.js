5348669651f7d3e21e85abef2ccec02e11e940e4
Rodolfo Florentin <152344472+REDF44@users.noreply.github.com>
2025-08-06 18:22:01 -0300

    multiplicacion de un numero por una lista

---
+++ 8kyo/x2lista.js
@@ -0,0 +1,7 @@
+/*Given an array of integers, return a new array with each value doubled.*/
+function maps(lista){
+    for (c = 0; c < lista.length; c++){
+        lista[c]= lista[c]*2;
+    }
+    return lista;
+}
